9c44c0a7e57e38225740e25b59342f23a5156b4f
vbchauhan <vbchauhan@gmail.com>
2014-08-21 19:56:14 -0400

    Update Main.js
    tp commit

--- app/view/Main.js
+++ app/view/Main.js
@@ -22,7 +22,7 @@ Ext.define('DriveItGreen.view.Main', {
 
                 },
 
-                { //Nav Panel - 3 : Social 
+                { //Nav Panel - 3 : Social //comments
                     xtype: 'nestedlist',
                     title: 'Social',
                     iconCls: 'user',
